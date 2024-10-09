d6c3097c8a965a12388d77fd407a566cf6116d25
Alex-Liang-ICD201-01 <alex.liang3@student.tdsb.on.ca>
2024-10-09 13:43:56 +0000

    commit

--- PP2_1.py
+++ PP2_1.py
@@ -3,10 +3,21 @@
 def q1(): 
   #Write Assignment code here
 
+num = int(input("Input a number: "))
+if num % 2 == 1:
+  print(f'{num} is odd')
+if num % 2 == 0: 
+  print(f'{num} is even')
 
 def q2(): 
   #Write Assignment code here
-
+  student = "student"
+  teacher = "teacher"
+name = str(input("Input a name: "))
+if name != "Kalisz":
+  print(student)
+if name = "Kalisz":
+  print(teacher)
 
 
 #Do not alter the following code
